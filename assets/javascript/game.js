f390aafacb6910dc0dcaea798b1f843ee6ca966f
JoeySuc89 <jsucameli1@gmail.com>
2019-03-15 19:55:59 -0400

    MVP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,73 @@
 // Set the global variabes for the games different counters.
-var scoreCounter = "";
+var scoreCounter = 0;
 var winsCounter = 0;
 var lossesCounter = 0;
-var randomScoreCounter = "";
+var crystalValueGreen = Math.floor(Math.random() * 12) + 1;
+var crystalValueBlue = Math.floor(Math.random() * 12) + 1;
+var crystalValuePurple = Math.floor(Math.random() * 12) + 1;
+var crystalValueAmber = Math.floor(Math.random() * 12) + 1;
+var randomScoreCounter = Math.floor(Math.random() * (120 - 19) + 19);
+$("#randomCounter").text(randomScoreCounter);
+console.log(randomScoreCounter)
 
 //Create a randomNumber loop to change the value of the crystals.
 $(document).ready(function() {
-$("#randomCounter").text(randomScoreCounter);
-$("#scoreCounter").text(scoreCounter);
-var numberOptions = Math.floor(Math.random() * 12) + 1;
+
+  $("#greenCrystal").on("click", function() {
+    scoreCounter += crystalValueGreen;
+    $("#scoreCounter").text("Your score: " + scoreCounter);
+    winsLossesScore();
+    console.log(crystalValueGreen)
+  });
 
 
-    $(".crystal-images").on("click", function(){
-    var crystalValue = ($(this).attr("data-crystalValue"));
-    crystalValue = parseInt(crystalValue);
-    scoreCounter += crystalValue;
-    $(".crystal-images").attr("data-crystalValue", numberOptions);
+  $("#blueCrystal").on("click", function() {
+    scoreCounter += crystalValueBlue;
+    $("#scoreCounter").text("Your score: " + scoreCounter);
+    winsLossesScore();
+    console.log(crystalValueBlue)
+  });
 
-    console.log(crystalValue);
-  })
+  $("#purpleCrystal").on("click", function() {
+    scoreCounter += crystalValuePurple;
+    $("#scoreCounter").text("Your score: " + scoreCounter);
+    winsLossesScore();
+    console.log(crystalValuePurple)
+  });
+
+  $("#amberCrystal").on("click", function() {
+    scoreCounter += crystalValueAmber;
+    $("#scoreCounter").text("Your score: " + scoreCounter);
+    winsLossesScore();
+    console.log(crystalValueAmber)
+  });
+
+
+
+   console.log(winsLossesScore);
 })
+
+function reset() {
+  randomScoreCounter = Math.floor(Math.random() * (120 - 19) + 19);
+  $("#randomCounter").text(randomScoreCounter);
+  crystalValueGreen = Math.floor(Math.random() * 12) + 1;;
+  crystalValueBlue = Math.floor(Math.random() * 12) + 1;;
+  crystalValuePurple = Math.floor(Math.random() * 12) + 1;;
+  crystalValueAmber = Math.floor(Math.random() * 12) + 1;;
+  scoreCounter = 0;
+}
+function winsLossesScore() {
+  console.log(`scoreCounter ${scoreCounter} randomCounter: ${randomScoreCounter}`)
+  if (scoreCounter == randomScoreCounter){
+    winsCounter++;
+    $("#winsCounter").html("Wins: " + winsCounter);
+    reset();
+    console.log(winsCounter);
+  }
+  if  (scoreCounter > randomScoreCounter) {
+    lossesCounter++;
+    $("#lossCounter").html("Losses: " + lossesCounter);
+    reset();
+    console.log(lossCounter);
+  }
+}
